0caf85ad0304f3969e4ee2c57964e99efd50bdf6
xanderblinov <xanderblinov@gmail.com>
2015-03-10 19:17:04 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -56,12 +56,12 @@ function onPushwooshAndroidInitialized(pushToken)
 }
 
 function initPushwoosh() {
-	console.log('initPushwoosh');
+	console.warn('initPushwoosh');
 	var pushNotification = window.plugins.pushNotification;
 	
-	console.log('registerPushwooshAndroid');
+	console.warn('registerPushwooshAndroid');
 	registerPushwooshAndroid();
-	console.log('onDeviceReady');
+	console.warn('onDeviceReady');
 	pushNotification.onDeviceReady({ projectid: "387050748417", appid : "A0443-C41F6" });
 }
 
